5d4f00da60982321552241085c88ef094a500718
apionila <gelagamkrelidze@gmai.com>
2023-07-21 23:31:43 +0400

    dey5

---
+++ dey4/dey5/dey5.py
@@ -0,0 +1,11 @@
+my_name = input( "sheiyvanet tqveni saxeli: ")
+my_surname = input( "sheiyvanet tqveni gvari: ")
+my_age = input( "sheiyvanet tqveni asaki: ")
+if "gela" in my_name :
+    print ("heloo gela")
+else :
+    print("serverze shesasvlelad aucilebelia gerqvat gela")
+print("my name is {} my surname is {} my age is {}". format ("gela","gamkrelidze","21"))
+after_3_years_my_age_will_be= int(my_age)+(3)
+if "21" in my_age :
+    print(("3 wlis shedeg chemi asaki iqneba :{}"). format("24")) 
